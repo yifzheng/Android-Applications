28d4c6cb3a2e5f2f80371d61c972ec755602260a
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-02-17 19:19:19 -0500

    completed basic functionalities of the calculator app

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,10 +8,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private double num1 = 0, num2 = 0, result = 0;
     private char operator;
+    private boolean gotOperator, gotResult; // for num2
     private String mathString;
     private TextView mathText;
     private Button btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnClear, btnNegate, btnMod, btnDiv, btnMulti, btnAdd, btnSub, btnEqual, btnPeriod;
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         /* Initialize buttons*/
         btnZero = (Button) findViewById(R.id.btnZero);
         btnOne = (Button) findViewById(R.id.btnOne);
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         btnPeriod = (Button) findViewById(R.id.btnPeriod);
         // initialize textview
         mathText = (TextView) findViewById(R.id.calcText);
+        gotOperator = false;
 
         // IF NUMBER IS CLICKED APPEND NUMBER TO TEXT VIEW WHILE LENGTH OF TEXT < 9
         btnZero.setOnClickListener(new View.OnClickListener() {
@@ -62,26 +66,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("1");
-                    } else {
-                        mathString += "1";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("1");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("1");
+                        } else {
+                            mathString += "1";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("2");
-                    } else {
-                        mathString += "2";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("2");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("2");
+                        } else {
+                            mathString += "2";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
             }
@@ -90,98 +107,147 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("3");
-                    } else {
-                        mathString += "3";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("3");
+                    gotResult = false;
+                    gotOperator = false; // TextView is cleared
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("3");
+                        } else {
+                            mathString += "3";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("4");
-                    } else {
-                        mathString += "4";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("4");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("4");
+                        } else {
+                            mathString += "4";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnFive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("5");
-                    } else {
-                        mathString += "5";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("5");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("5");
+                        } else {
+                            mathString += "5";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnSix.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("6");
-                    } else {
-                        mathString += "6";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("6");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("6");
+                        } else {
+                            mathString += "6";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnSeven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("7");
-                    } else {
-                        mathString += "7";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("7");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("7");
+                        } else {
+                            mathString += "7";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnEight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("8");
-                    } else {
-                        mathString += "8";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("8");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("8");
+                        } else {
+                            mathString += "8";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnNine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (mathString.length() < 9) {
-                    if (mathString.equals("0")) {
-                        mathText.setText("9");
-                    } else {
-                        mathString += "9";
-                        mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("9");
+                    gotOperator = false; // TextView is cleared
+                    gotResult = false;
+                } else {
+                    if (mathString.length() < 9) {
+                        if (mathString.equals("0")) {
+                            mathText.setText("9");
+                        } else {
+                            mathString += "9";
+                            mathText.setText(mathString);
+                        }
                     }
                 }
+
             }
         });
         btnClear.setOnClickListener(new View.OnClickListener() {
@@ -197,10 +263,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mathString = mathText.getText().toString();
-                if (!mathString.contains(".")) {
-                    mathString += ".";
-                    mathText.setText(mathString);
+                if (gotOperator || gotResult) {
+                    mathText.setText("0.");
+                    gotOperator = false;
+                    gotResult = false;
+                } else {
+                    if (!mathString.contains(".")) {
+                        mathString += ".";
+                        mathText.setText(mathString);
+                    }
                 }
+
             }
         });
         btnAdd.setOnClickListener(new View.OnClickListener() {
@@ -208,44 +281,90 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 num1 = Double.parseDouble(mathText.getText().toString());
                 operator = '+';
-                mathText.setText("");
+                gotOperator = true;
             }
         });
-        btnEqual.setOnClickListener(new View.OnClickListener() {
+        btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num2 = Double.parseDouble(mathText.getText().toString());
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    default:
-                        break;
-                }
-                if (isDouble(String.valueOf(result))) {
-                    mathText.setText(String.valueOf(result));
-                } else {
-                    mathText.setText(String.valueOf((Integer.parseInt(String.valueOf(result)))));
-                }
-                num2 = 0;
-                num1 = result;
+                num1 = Double.parseDouble(mathText.getText().toString());
+                operator = '-';
+                gotOperator = true;
+            }
+        });
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Double.parseDouble(mathText.getText().toString());
+                operator = '*';
+                gotOperator = true;
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Double.parseDouble(mathText.getText().toString());
+                operator = '/';
+                gotOperator = true;
             }
         });
         // IF A MATH FUNCTION IS CLICKED,UPDATE NUM1
         btnMod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Clicking Modular Button", Toast.LENGTH_SHORT).show();
+                num1 = Double.parseDouble(mathText.getText().toString());
+                operator = '%';
+                gotOperator = true;
             }
         });
+        btnNegate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mathString = mathText.getText().toString();
+                double mathNum = Double.parseDouble(mathString);
+                if (isInteger(mathNum)) {
+                    mathText.setText(String.valueOf((int) mathNum * -1));
+                } else {
+                    mathText.setText(String.valueOf((double) mathNum * -1));
+                }
+            }
+        });
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num2 = Double.parseDouble(mathText.getText().toString());
+                gotResult = true;
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    case '%':
+                        result = num1 % num2;
+                        break;
+                    default:
+                        break;
+                }
+                if (isInteger(result)) {
+                    mathText.setText(String.valueOf((int) result));
+                } else {
+                    mathText.setText(String.valueOf((double) result));
+                }
+                num1 = (double) result;
+            }
+        });
+
     }
 
-    public boolean isDouble(String num) {
-        try {
-            Double.parseDouble(num);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+    public boolean isInteger(Double num) {
+        return Math.ceil(num) == Math.floor(num);
     }
 }
