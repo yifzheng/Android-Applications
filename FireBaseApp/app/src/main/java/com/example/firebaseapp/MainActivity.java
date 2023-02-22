e66298dea2b0379a3b0a92cb2630439c790af1c7
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-02-22 17:53:24 -0500

    added READ function

--- FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -8,12 +8,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_TITLE = "title", KEY_DESCRIPTION = "description";
 
     private EditText editTextTitle, editTextDescription;
-    private Button saveButton;
+    private Button saveButton, loadButton;
+    private TextView loadContentView;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -38,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         editTextTitle = (EditText) findViewById(R.id.edit_title_view);
         editTextDescription = (EditText) findViewById(R.id.edit_description_view);
         saveButton = (Button) findViewById(R.id.save_btn);
+        loadButton = (Button) findViewById(R.id.load_btn);
+        loadContentView = (TextView) findViewById(R.id.textview_content_load);
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,7 +58,30 @@ public class MainActivity extends AppCompatActivity {
                 db.collection("Notebook").add(note).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(MainActivity.this, "Successfully saved", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Successfully saved \n ID: " + documentReference.getId(), Toast.LENGTH_SHORT).show();
+                        editTextDescription.setText(""); // clear edit view
+                        editTextTitle.setText(""); // clear edit view
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+            }
+        });
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.collection("Notebook").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot document: queryDocumentSnapshots)
+                        {
+                           Log.i(TAG, document.getId() + " => " + document.getData());
+                        }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
